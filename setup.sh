ae7f38602adb93335b285ba16f888a10e49bd86d
Valerii Iatsko <viatsko@viatsko.me>
2021-04-27 00:17:11 +0200

    Added Paul's .osx script w/ personal modifications

--- setup.sh
+++ setup.sh
@@ -3,4 +3,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until setup has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+. _dock_cleanup.sh
+. _brew_update_or_install.sh
+. _osx.sh
 
