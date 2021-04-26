54ccbf619f0e6b4d4b45a27a4d5d076086fe08b9
Valerii Iatsko <viatsko@viatsko.me>
2021-04-27 00:25:38 +0200

    Old apps install script

---
+++ _brew_install_apps.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+brew tap dart-lang/dart
+
+brew install dart \
+	fzf \
+	coreutils \
+	moreutils \
+	findutils \
+	gnu-sed \
+	nvm \
+	pstree \
+	openjdk \
+	the_silver_searcher \
+	ffmpeg --with-libvpx \
+	terminal-notifier
+
+brew install --cask spotify \
+	telegram \
+	iterm2 \
+	dropbox \
+	discord \
+	flux \
+	vlc \
+	jetbrains-toolbox
+
+# OpenJDK post-install
+sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+
+brew cleanup
+
