33644511dc12572aeb15628fdbfeabfb7ef0bf06
Valerii Iatsko <viatsko@viatsko.me>
2021-04-27 08:05:30 +0200

    Updated setup.sh (accounting for CygWin)

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until setup has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# As some files might have been committed from cygwin, ensure exec-bit
+chmod +x *.sh
+
+# Run step-by-step scripts to set the machine up
 . _dock_cleanup.sh
 . _brew_update_or_install.sh
 . _brew_install_apps.sh
