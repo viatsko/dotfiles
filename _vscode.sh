f46e60beb06fa3c6861af515a088876cc5864673
Valerii Iatsko <viatsko@viatsko.me>
2021-04-28 22:58:27 +0200

    Added overnight theme for vscode

--- _vscode.sh
+++ _vscode.sh
@@ -11,3 +11,14 @@ ln -s $DIR/Code/keybindings.json $HOME/Library/Application\ Support/Code/User/ke
 
 rm -rf $HOME/Library/Application\ Support/Code/User/snippets
 ln -s $DIR/Code/snippets/ $HOME/Library/Application\ Support/Code/User
+
+pkglist=(
+eamodio.gitlens
+esbenp.prettier-vscode
+ms-vscode-remote.remote-ssh
+cev.overnight
+)
+
+for i in ${pkglist[@]}; do
+  code --install-extension $i
+done
