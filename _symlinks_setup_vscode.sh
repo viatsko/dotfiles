3dc2640f5cea142d29f9b1e0971e605cd985e27b
Valerii Iatsko <viatsko@viatsko.me>
2021-04-28 22:54:18 +0200

    VSCode setup stuff

---
+++ _symlinks_setup_vscode.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $DIR
+
+rm -f $HOME/Library/Application\ Support/Code/User/settings.json
+ln -s $DIR/Code/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+rm -f $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -s $DIR/Code/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+rm -rf $HOME/Library/Application\ Support/Code/User/snippets
+ln -s $DIR/Code/snippets/ $HOME/Library/Application\ Support/Code/User
