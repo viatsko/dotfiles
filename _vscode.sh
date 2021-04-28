5c39e2b4a7f0171371881182c3659a416c484057
Valerii Iatsko <viatsko@viatsko.me>
2021-04-28 22:55:46 +0200

    More space

--- _vscode.sh
+++ _vscode.sh
@@ -5,7 +5,9 @@ cd $DIR
 
 rm -f $HOME/Library/Application\ Support/Code/User/settings.json
 ln -s $DIR/Code/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+
 rm -f $HOME/Library/Application\ Support/Code/User/keybindings.json
 ln -s $DIR/Code/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+
 rm -rf $HOME/Library/Application\ Support/Code/User/snippets
 ln -s $DIR/Code/snippets/ $HOME/Library/Application\ Support/Code/User
